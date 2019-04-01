98d43082d9802840d9acc048afd763b20dbedf68
Alex Thacker <thacker.alex@yahoo.com>
2019-04-01 09:30:37 -0500

    setup

---
+++ src/scripts/main.js
@@ -0,0 +1,64 @@
+// import createCar from "./createCar"
+
+// const garage = []
+
+
+// const mustang = createCar("ford", "mustang");
+
+
+// console.log(mustang)
+
+// garage.push(mustang)
+
+// console.log(Object.entries(mustang))
+
+
+// /*
+//     Author: your name here
+//     Name: main.js
+//     Purpose: Entry point of our application
+// */
+// // import createCar from "./createCar"
+
+
+// // const garage = []
+
+// // Create two cars using the function you imported
+// // const mustang = createCar("Ford", "Mustang")
+// const accord = createCar("Honda", "Accord")
+
+// // Drive the cars for a while
+// // console.log(mustang.drive("the grocery store"))
+// // console.log(accord.drive("Indianapolis"))
+
+// // Park the cars in the garage
+// garage.push(mustang)
+// garage.push(accord)
+
+// console.table(garage);
+
+
+
+
+/*
+    Author: your name here
+    Name: main.js
+    Purpose: Entry point of our application
+*/
+import carFactory from "./createCar"
+import garageFactory from "./garageFactory"
+
+// Create two cars using the function you imported
+const mustang = carFactory("Ford", "Mustang")
+const accord = carFactory("Honda", "Accord")
+const santafe = carFactory("Hyundai", "Santa Fe")
+const sierra = carFactory("GMC", "Sierra")
+
+/// Store the cars in the garage
+garageFactory.store(mustang)
+garageFactory.store(accord)
+garageFactory.store(santafe)
+garageFactory.store(sierra)
+
+console.table(garageFactory.inventory())
+console.table(garageFactory.retrieve(sierra))
